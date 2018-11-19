77e63bb1ed70e2c94ef90ba9ffe713b08e63897c
Eric Xia <exia@lcs.on.ca>
2018-11-19 09:23:26 -0500

    Finished the square and rectangle questions, completed parallelogram and triangle

--- Formulas-And-Functions/main.swift
+++ Formulas-And-Functions/main.swift
@@ -12,3 +12,25 @@ import Foundation
 let a = 7.0
 let areaOfSquare = forSquareWith(sideLength: a)
 print("The area of a square with side length \(a) is \(areaOfSquare)")
+
+// Calculate the area of a rectangle
+let m = 10.0
+let n = 2.0
+let areaofRectangle = forRectangleWith(length: m, width: n)
+print(areaofRectangle)
+
+// Calculate the area of a parallelogram
+let x = 3.0
+let y = 2.0
+let areaofParallelogram = forParallelogramWith(base: x, height: y)
+print("Area of parallelogram is: \(areaofParallelogram)")
+
+// Calculate the area of a triangle
+let e = 4.0
+let f = 8.0
+let areaofTriangle = forTriangleWith(base: e, height : f)
+print("Area of triangle is: \(areaofTriangle)")
+
+// Calculate the area of a circle
+//let r = 5.0
+
